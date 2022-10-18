1f67455998babe7e525884ad520d94434509ed30
SungSoo Kim <97649989+benkim077@users.noreply.github.com>
2022-10-18 23:06:03 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 448 ms, Memory: 241040 KB -BaekjoonHub

--- 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.py
@@ -1,17 +1,17 @@
-st_A = [0] + list(input())
-st_B = [0] + list(input())
+string_A = input()
+string_B = input()
 
-A = len(st_A)
-B = len(st_B)
-dp = [[0] * (B + 1) for _ in range(A + 1)]
+len_A = len(string_A)
+len_B = len(string_B)
+dp = [[0] * (len_B + 1) for _ in range(len_A + 1)]
 
 mx = 0
-for a in range(1, A):
-    for b in range(1, B):
-        if st_A[a] == st_B[b]:
-            dp[a][b] = dp[a - 1][b - 1] + 1
+for a in range(len_A):
+    for b in range(len_B):
+        if string_A[a] == string_B[b]:
+            dp[a + 1][b + 1] = dp[a][b] + 1
         else:
-            dp[a][b] = 0
-        mx = max(mx, dp[a][b])
+            dp[a + 1][b + 1] = 0
+        mx = max(mx, dp[a + 1][b + 1])
 
 print(mx)
