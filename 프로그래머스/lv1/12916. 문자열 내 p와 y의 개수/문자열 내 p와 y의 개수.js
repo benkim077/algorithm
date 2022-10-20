7ca4b009644bf58bf65031a06e0645826f2d341c
SungSoo Kim <97649989+benkim077@users.noreply.github.com>
2022-10-20 20:01:03 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 3.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
+++ 프로그래머스/lv1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.js
@@ -1,24 +1,17 @@
 function solution(s){
     var answer = true;
 
-    let cnt_p = 0
-    let cnt_y = 0
-    
-    // 문자열은 이터러블인가? OK
-    for (const char of s) {
-        if (char === 'p' || char === 'P') {
-            cnt_p += 1
-        } else if (char === 'y' || char === 'Y') {
-            cnt_y += 1
+    cnt = s.split('').reduce((total, x) => {
+        if (x == 'p' || x == 'P') {
+            return total + 1
+        } else if (x == 'y' || x == 'Y') {
+            return total - 1
+        } else {
+            return total
         }
-    }
+    }, 0)
     
-    console.log(cnt_p, cnt_y)
-    if (cnt_p === cnt_y) {
-        answer = true
-    } else {
-        answer = false
-    }
-
+    console.log(cnt)
+    answer = cnt ? false : true
     return answer;
 }
