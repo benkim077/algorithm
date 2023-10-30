0759243e77d562f53d00d35a3eabb92676ff188d
SungSoo Kim <123vfcx@gmail.com>
2023-10-30 17:44:34 +0900

    [Bronze V] Title: A+B - 5, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a, b;
+
+int main() {
+    while(true) {
+        cin >> a >> b;
+        if(a == 0 && b == 0) break;
+
+        cout << a + b << '\n';
+    }
+    return 0;
+}
