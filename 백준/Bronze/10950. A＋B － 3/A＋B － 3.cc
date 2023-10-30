7799d50f925cd6070da6750c432f5f132cf89096
SungSoo Kim <123vfcx@gmail.com>
2023-10-30 17:15:37 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t, a, b;
+
+int main() {
+    cin >> t;
+    for(int i = 0; i < t; i++) {
+        cin >> a >> b;
+        cout << a + b << '\n';
+    }
+}
