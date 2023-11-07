7d5e9b6863ced48ce11c383b671a75b0aa6fdf94
SungSoo Kim <123vfcx@gmail.com>
2023-11-07 11:36:35 +0900

    [Silver V] Title: 막대기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, ret;
+
+int main() {
+    cin >> n;
+    for(int i = 0; i < 7; i++) {
+        if(n & (1 << i)) ret++;
+    }
+    cout << ret << '\n';
+    return 0;
+}
