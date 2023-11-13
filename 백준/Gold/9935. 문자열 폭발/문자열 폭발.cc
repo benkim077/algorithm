1e96f511fc72afcc015a512b8b87698e14f04e36
SungSoo Kim <123vfcx@gmail.com>
2023-11-13 12:16:21 +0900

    [Gold IV] Title: 문자열 폭발, Time: 88 ms, Memory: 7796 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string a, b, ret;
+
+int main() {
+    cin >> a >> b;
+    for(char c : a) {
+        ret += c;
+        if( ret.size() >= b.size() && 
+            ret.substr(ret.size() - b.size(), b.size()) == b) {
+                ret.erase(ret.size() - b.size(), b.size());
+        }
+    }
+    if(ret.empty()) cout << "FRULA" << '\n';
+    else cout << ret << '\n';
+    return 0;
+}
