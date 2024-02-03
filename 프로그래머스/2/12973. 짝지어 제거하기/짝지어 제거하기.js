56de7b1071eff41579748c639e264182568f09eb
SungSoo Kim <123vfcx@gmail.com>
2024-02-03 11:03:24 +0900

    [level 2] Title: 짝지어 제거하기, Time: 75.88 ms, Memory: 46.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.js
@@ -0,0 +1,8 @@
+function solution(s) {
+    const stk = [];
+    for(let i = 0; i < s.length; i++) {
+        if(stk.length && stk.at(-1) === s[i]) stk.pop();
+        else stk.push(s[i]);
+    }
+    return stk.length ? 0 : 1;
+}
