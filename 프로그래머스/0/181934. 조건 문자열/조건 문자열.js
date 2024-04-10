a0261addfb2a43d467a466a525ab4c6454c177d9
SungSoo Kim <123vfcx@gmail.com>
2024-04-10 09:48:32 +0900

    [level 0] Title: 조건 문자열, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181934. 조건 문자열/조건 문자열.js
@@ -0,0 +1,18 @@
+function solution(ineq, eq, n, m) {
+    if (ineq === "<" && eq === "=") {
+        if (n <= m) return 1;
+        else return 0;
+    } 
+    if (ineq === "<" && eq === "!") {
+        if (n < m) return 1;
+        else return 0;
+    }
+    if (ineq === ">" && eq === "=") {
+        if (n >= m) return 1;
+        else return 0;
+    }
+    if (ineq === ">" && eq === "!") {
+        if (n > m) return 1;
+        else return 0;
+    }
+}
